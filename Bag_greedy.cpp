03ab1d2ca6b15ad6f5c4ce5d92e52b0331f422e4
Neo0 <wjh100@live.cn>
2016-04-13 21:37:06 +0800

    finished code

--- Bag_greedy.cpp
+++ Bag_greedy.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <algorithm>
+
+
+
+bool compare(int a,int b)
+{
+  return a>b;
+}
 
 
 template<class T>
-
 class Knapsack
 {
     public:
-        Knapsack(int mSize.float cap,float *wei,T *prof);
-        void GreedyKnapsack(flaot *x);
+        Knapsack(int mSize,float cap,float *wei,T *prof);
+        void sort_weigh();
+        void GreedyKnapsack(float *x);
     
     private:
         float m,*w;
@@ -16,17 +24,24 @@ class Knapsack
 };
 
 template<class T>
-Knapsack<T>::Knapsack()
+Knapsack<T>::Knapsack(int mSize,float cap,float *wei,T *prof)
+{
+    float m = mSize;
+    float *w = wei;
+    T *p = prof;
+    int n = sizeof(wei)/sizeof(wei[0]);
+}
 
 template<class T>
 void Knapsack<T>::GreedyKnapsack(float *x)
 {
     float u = m;
-    for (int i = 0;i<=n;i++)
+    for (int i = 0;i<n;i++) x[i] = 0;
+    for (i = 0;i<n;i++)
     {
         if (w[i]>u) break;
         x[i]=1.0;
-        u = u[i];
+        u = u - w[i];
     }
     if (i<n) x[i]=u/w[i];
 }
@@ -34,5 +49,10 @@ void Knapsack<T>::GreedyKnapsack(float *x)
 template<class T>
 void Knapsack<T>::sort_weigh()
 {
-    
+    sort(w,w+n,compare);
+}
+
+int main(void)
+{
+    return 0;
 }
