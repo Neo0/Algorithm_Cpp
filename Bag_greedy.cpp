2948d6961b3949c459518ceb9582e3020b0235a9
Neo0 <wjh100@live.cn>
2016-04-13 21:11:04 +0800

    complete file

---
+++ Bag_greedy.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+template<class T>
+
+class Knapsack
+{
+    public:
+        Knapsack(int mSize.float cap,float *wei,T *prof);
+        void GreedyKnapsack(flaot *x);
+    
+    private:
+        float m,*w;
+        T *p;
+        int n;
+};
+
+template<class T>
+void Knapsack<T>::GreedyKnapsack(float *x)
+{
+    float u = m;
+    for
+}
